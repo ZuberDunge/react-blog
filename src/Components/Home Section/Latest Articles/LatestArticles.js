d137af4999bb9bf99097cc25d63596ed59527f75
ZuberDunge <iamZuberDunge@gmail.com>
2021-11-16 12:57:52 +0530

    update :dynamic

--- src/Components/Home Section/Latest Articles/LatestArticles.js
+++ src/Components/Home Section/Latest Articles/LatestArticles.js
@@ -6,7 +6,22 @@ import HomeArticlesImage1 from "../../../Images/articles-image.PNG"
 import { useParams, useNavigate, Link } from "react-router-dom";
 
 
-
+function createTopPOst(data) {
+    return <div className="top-post-section">
+        <img src={data.imgURL} alt="kuchbhi" />
+        <div className="top-post-section-home-flex">
+            <div>
+                <Link to={`/${data.category}/${data.id}`}> <div className="latest-top-post-section-title-1">
+                    {data.title.substring(0, 42)}
+                </div> </Link>
+            </div>
+            <div>
+                <span className="latest-home-type">{data.category}</span>
+                <span className="latest-home-date">{data.time}</span>
+            </div>
+        </div>
+    </div>
+}
 
 
 
@@ -95,61 +110,20 @@ function LatestArticles() {
                     <div className="heading-home-latest" >Top Posts</div>
 
                     <div className="top-post-home margtb2rem">
-                        <img src={HomeArticlesImage1} alt="kuch bhi" />
+                        <img src={LatestArticlesArray[0].imgURL} alt="kuch bhi" />
 
-                        <div className="latest-post-title-1 margtop1rem">
-                            Joshua Tree Overnight Adventure
-                        </div>
+                        <Link to={`/${LatestArticlesArray[0].category}/${LatestArticlesArray[0].id}`}>    <div className="latest-post-title-1 margtop1rem">
+                            {LatestArticlesArray[0].title}
+                        </div></Link>
 
                         <div className="margtop1rem">
-                            <span className="latest-home-type ">Travel</span>
-                            <span className="latest-home-date">/ August 21 2017</span>
+                            <span className="latest-home-type ">{LatestArticlesArray[0].category}</span>
+                            <span className="latest-home-date">{LatestArticlesArray[0].time}</span>
                         </div>
 
-                        <div className="top-post-section">
-                            <img src={HomeArticlesImage1} alt="kuchbhi" />
-                            <div className="top-post-section-home-flex">
-                                <div>
-                                    <div className="latest-top-post-section-title-1">
-                                        Joshua Tree Overnight Adventure
-                                    </div>
-                                </div>
-                                <div>
-                                    <span className="latest-home-type">Travel</span>
-                                    <span className="latest-home-date">/ August 21 2017</span>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div className="top-post-section">
-                            <img src={HomeArticlesImage1} alt="kuchbhi" />
-                            <div className="top-post-section-home-flex">
-                                <div>
-                                    <div className="latest-top-post-section-title-1">
-                                        Joshua Tree Overnight Adventure
-                                    </div>
-                                </div>
-                                <div>
-                                    <span className="latest-home-type">Travel</span>
-                                    <span className="latest-home-date">/ August 21 2017</span>
-                                </div>
-                            </div>
-                        </div>
+                        {LatestArticlesArray.slice(6, 9).map(createTopPOst)}
 
-                        <div className="top-post-section">
-                            <img src={HomeArticlesImage1} alt="kuchbhi" />
-                            <div className="top-post-section-home-flex">
-                                <div>
-                                    <div className="latest-top-post-section-title-1">
-                                        Joshua Tree Overnight Adventure
-                                    </div>
-                                </div>
-                                <div>
-                                    <span className="latest-home-type">Travel</span>
-                                    <span className="latest-home-date">/ August 21 2017</span>
-                                </div>
-                            </div>
-                        </div>
 
 
 
